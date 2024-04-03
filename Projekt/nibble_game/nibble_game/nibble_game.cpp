d7966d618ce25fb6ab4790474795a68f743ac3f0
antosiowsky <ja305476@student.polsl.pl>
2024-04-03 21:38:55 +0200

    basic interface

--- Projekt/nibble_game/nibble_game/nibble_game.cpp
+++ Projekt/nibble_game/nibble_game/nibble_game.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 
 int main(void) {
+    int skill_level;
+    char increase_speed, mono_colo;
+
+    std::cout << "Skill level (1 to 100)? \n 1 = Novice \n 90 = Expert \n 100 = Twidlle fingers \n";
+    std::cin >> skill_level;
+
+    std::cout << "Increase game speed during play (Y or N)? ";
+    std::cin >> increase_speed;
+    
+    std::cout << "Monochrome or color monitor (M or C)? ";
+    std::cin >> mono_colo;
+
+
+
+    /*
     sf::RenderWindow window(sf::VideoMode(500, 200), "SFML works!"); //rozmiar okna 
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -17,6 +32,6 @@ int main(void) {
         window.draw(shape);
         window.display();
     }
-
+    */
     return 0;
 }
