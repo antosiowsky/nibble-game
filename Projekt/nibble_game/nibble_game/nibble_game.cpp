a5c81d2ab49bd63e1f51208ab92de4bf6570989f
antosiowsky <ja305476@student.polsl.pl>
2024-04-09 12:52:12 +0200

    interface

--- Projekt/nibble_game/nibble_game/nibble_game.cpp
+++ Projekt/nibble_game/nibble_game/nibble_game.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main(void) {
+    /*
     int skill_level;
     char increase_speed, mono_colo;
 
@@ -13,25 +14,86 @@ int main(void) {
     
     std::cout << "Monochrome or color monitor (M or C)? ";
     std::cin >> mono_colo;
+    
 
+    */
 
-
-    /*
-    sf::RenderWindow window(sf::VideoMode(500, 200), "SFML works!"); //rozmiar okna 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    // Ustaw odstęp od krawędzi okna
+    const float padding = 15.f;
+    const float topPadding = 40.f;
+    const float thickness = 10.f;
+    int initialWidth = 850;
+    int initialHeight = 575;
+    
+    // Utwórz okno o rozmiarze 800x500 pikseli
+    sf::RenderWindow window(sf::VideoMode(850, 575), "Okno SFML z niebieskim tłem");
+    sf::View view(sf::FloatRect(0, 0, initialWidth, initialHeight));
+    // Główna pętla programu
     while (window.isOpen()) {
+        // Sprawdź wszystkie zdarzenia, które wystąpiły od ostatniego cyklu pętli
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            // Obsłuż zamknięcie okna
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            if (event.type == sf::Event::Resized) {
+                float aspectRatio = static_cast<float>(event.size.width) / event.size.height;
+                if (aspectRatio > initialWidth / initialHeight) {
+                    // Szerokość okna jest większa - skaluj względem szerokości
+                    float newWidth = initialWidth * (event.size.height / initialHeight);
+                    view.setSize(newWidth * aspectRatio, event.size.height);
+                }
+                else {
+                    // Wysokość okna jest większa lub równa - skaluj względem wysokości
+                    float newHeight = initialHeight * (event.size.width / initialWidth);
+                    view.setSize(event.size.width, newHeight / aspectRatio);
+                }
+
+                window.setView(view);
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        // Wyczyść ekran ustawiając tło na kolor niebieski
+        sf::Color bG(0, 0, 128);
+        window.clear(bG);
+
+
+
+
+        // Rysuj ramki dookoła okna
+
+        sf::RectangleShape topBorder(sf::Vector2f(window.getSize().x - 2 * padding, thickness));
+        topBorder.setPosition(padding, topPadding);
+        topBorder.setFillColor(sf::Color::Red);
+
+        sf::RectangleShape bottomBorder(sf::Vector2f(window.getSize().x - 2 * padding, thickness));
+        bottomBorder.setPosition(padding, window.getSize().y - thickness - padding);
+        bottomBorder.setFillColor(sf::Color::Red);
+
+        sf::RectangleShape leftBorder(sf::Vector2f(thickness, window.getSize().y - 2 * padding - topPadding/1.5 ));
+        leftBorder.setPosition(padding, topPadding);
+        leftBorder.setFillColor(sf::Color::Red);
+
+        sf::RectangleShape rightBorder(sf::Vector2f(thickness, window.getSize().y - 2 * padding - topPadding/1.5));
+        rightBorder.setPosition(window.getSize().x - thickness - padding, topPadding);
+        rightBorder.setFillColor(sf::Color::Red);
+
+        window.draw(topBorder);
+        window.draw(bottomBorder);
+        window.draw(leftBorder);
+        window.draw(rightBorder);
+
+
+
+
+        // Wyświetl zawartość okna
         window.display();
     }
-    */
+
+    return 0;
+
+    
     return 0;
 }
