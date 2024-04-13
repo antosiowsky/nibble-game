34b1ed12fb2b44ff3de0c5ac8a87eadea9d9ddd3
antosiowsky <ja305476@student.polsl.pl>
2024-04-13 12:23:17 +0200

    git

--- Projekt/nibble_game/nibble_game/nibble_game.cpp
+++ Projekt/nibble_game/nibble_game/nibble_game.cpp
@@ -1,7 +1,4 @@
-﻿
-
-
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
 #include <iostream>
 
 class Snake {
@@ -33,34 +30,19 @@ private:
 };
 
 int main(void) {
-    /*
-    int skill_level;
-    char increase_speed, mono_colo;
-
-    std::cout << "Skill level (1 to 100)? \n 1 = Novice \n 90 = Expert \n 100 = Twidlle fingers \n";
-    std::cin >> skill_level;
-
-    std::cout << "Increase game speed during play (Y or N)? ";
-    std::cin >> increase_speed;
-
-    std::cout << "Monochrome or color monitor (M or C)? ";
-    std::cin >> mono_colo;
-
-
-    */
-
     // Ustaw odstęp od krawędzi okna
     const float padding = 15.f;
     const float topPadding = 40.f;
     const float thickness = 10.f;
-    int initialWidth = 850;
-    int initialHeight = 575;
+    int initialWidth = 1600;
+    int initialHeight = 1000;
 
-    // Utwórz okno o rozmiarze 800x500 pikseli
-    sf::RenderWindow window(sf::VideoMode(850, 575), "Okno SFML z niebieskim tłem");
-    //sf::View view(sf::FloatRect(0, 0, initialWidth, initialHeight));
+    // Utwórz okno o rozmiarze 1600x1000 pikseli
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "Okno SFML z niebieskim tłem");
+
+    Snake snake(10, 20); // Utworzenie węża
+
     // Główna pętla programu
-    /*
     while (window.isOpen()) {
         // Sprawdź wszystkie zdarzenia, które wystąpiły od ostatniego cyklu pętli
         sf::Event event;
@@ -69,59 +51,6 @@ int main(void) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-
-            if (event.type == sf::Event::Resized) {
-                float aspectRatio = static_cast<float>(event.size.width) / event.size.height;
-                if (aspectRatio > initialWidth / initialHeight) {
-                    // Szerokość okna jest większa - skaluj względem szerokości
-                    float newWidth = initialWidth * (event.size.height / initialHeight);
-                    view.setSize(newWidth * aspectRatio, event.size.height);
-                }
-                else {
-                    // Wysokość okna jest większa lub równa - skaluj względem wysokości
-                    float newHeight = initialHeight * (event.size.width / initialWidth);
-                    view.setSize(event.size.width, newHeight / aspectRatio);
-                }
-
-                window.setView(view);
-            }
-        }
-        */
-        // Wyczyść ekran ustawiając tło na kolor niebieski
-
-
-    sf::Color bG(0, 0, 128);
-    window.clear(bG);
-
-    Snake snake(10, 20); // Utworzenie węża
-
-
-    // Rysuj ramki dookoła okna
-
-    sf::RectangleShape topBorder(sf::Vector2f(window.getSize().x - 2 * padding, thickness));
-    topBorder.setPosition(padding, topPadding);
-    topBorder.setFillColor(sf::Color::Red);
-
-    sf::RectangleShape bottomBorder(sf::Vector2f(window.getSize().x - 2 * padding, thickness));
-    bottomBorder.setPosition(padding, window.getSize().y - thickness - padding);
-    bottomBorder.setFillColor(sf::Color::Red);
-
-    sf::RectangleShape leftBorder(sf::Vector2f(thickness, window.getSize().y - 2 * padding - topPadding / 1.5));
-    leftBorder.setPosition(padding, topPadding);
-    leftBorder.setFillColor(sf::Color::Red);
-
-    sf::RectangleShape rightBorder(sf::Vector2f(thickness, window.getSize().y - 2 * padding - topPadding / 1.5));
-    rightBorder.setPosition(window.getSize().x - thickness - padding, topPadding);
-    rightBorder.setFillColor(sf::Color::Red);
-
-    /*
-
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
 
             // Obsługa ruchu węża za pomocą strzałek
             if (event.type == sf::Event::KeyPressed) {
@@ -144,23 +73,36 @@ int main(void) {
             }
         }
 
+        // Wyczyść ekran ustawiając tło na kolor niebieski
+        sf::Color bG(0, 0, 128);
+        window.clear(bG);
 
+        // Rysuj ramki dookoła okna
+        sf::RectangleShape topBorder(sf::Vector2f(window.getSize().x - 2 * padding, thickness));
+        topBorder.setPosition(padding, topPadding);
+        topBorder.setFillColor(sf::Color::Red);
+
+        sf::RectangleShape bottomBorder(sf::Vector2f(window.getSize().x - 2 * padding, thickness));
+        bottomBorder.setPosition(padding, window.getSize().y - thickness - padding);
+        bottomBorder.setFillColor(sf::Color::Red);
+
+        sf::RectangleShape leftBorder(sf::Vector2f(thickness, window.getSize().y - 2 * padding - topPadding / 1.5));
+        leftBorder.setPosition(padding, topPadding);
+        leftBorder.setFillColor(sf::Color::Red);
+
+        sf::RectangleShape rightBorder(sf::Vector2f(thickness, window.getSize().y - 2 * padding - topPadding / 1.5));
+        rightBorder.setPosition(window.getSize().x - thickness - padding, topPadding);
+        rightBorder.setFillColor(sf::Color::Red);
+
+        window.draw(topBorder);
+        window.draw(bottomBorder);
+        window.draw(leftBorder);
+        window.draw(rightBorder);
+
+        // Wyświetl zawartość okna
+        snake.draw(window);
+        window.display();
     }
-    */
-
-
-    window.draw(topBorder);
-    window.draw(bottomBorder);
-    window.draw(leftBorder);
-    window.draw(rightBorder);
-    // Wyświetl zawartość okna
-    snake.draw(window);
-    window.display();
-
-
-
-
 
     return 0;
-
 }
